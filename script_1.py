ff4eb4619d4f75c1f72bd5ce977616867b7e2b08
ishantgarg40 <ishantgarg40@gmail.com>
2019-07-10 23:32:37 +0530

    Add files via upload

---
+++ script_1.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup as bs
+# import urllib.requests
+
+try:
+    resp = requests.get("https://mailtrack.io/trace/link/cb91e92af171a0e317b132507bc57eb22066b02b?url=http%3A%2F%2Ffizz.phys.dal.ca%2F~atmos%2Fmartin%2F%3Fpage_id%3D140&userId=3966469&signature=25076cae96d597e1")
+    soup = bs(resp.text,"html.parser")
+    p_tags = soup.find_all('p')
+    for data in p_tags:
+        if "Satellite-Derived PM<sub>2.5</sub>, 2016, at 35% RH [ug/m3]" in str(data):
+            temp = data
+    temp = temp.find_all('a')
+    # print(temp)
+    for data in temp:
+        if "[.nc]" in str(data):
+            x = data
+            break
+
+    downloadUrl = x.get('href')
+    resp = requests.get(downloadUrl)
+    with open("raster.nc","wb") as f:
+        f.write(resp.content)
+except:
+    print("NetworkError or problem with the URL\n")
+    
